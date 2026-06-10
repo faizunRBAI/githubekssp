b6c6977ce3f0aa2a2c8f4518e43772b1d63bc530
faizunRBAI <faizun@royalbengal.ai>
2026-06-10 21:57:16 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/githubekssp/GithubeksspApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubekssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubeksspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubeksspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubekssp/GithubeksspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubekssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubeksspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
